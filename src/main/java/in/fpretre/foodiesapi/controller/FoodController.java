71d5430d1634cad0d38d34b5977128b5771e6d0a
FANNY PRETRE <fanny.pretre@gmail.com>
2025-06-27 12:48:19 +0200

    Add food working

--- src/main/java/in/fpretre/foodiesapi/controller/FoodController.java
+++ src/main/java/in/fpretre/foodiesapi/controller/FoodController.java
@@ -3,6 +3,7 @@ package in.fpretre.foodiesapi.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/api/foods")
 @AllArgsConstructor
+@CrossOrigin("*")
 
 // Création de la handler method
 
