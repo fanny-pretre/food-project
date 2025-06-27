d9e2b89adacc33e532d1564a4678d34c5c5dc141
FANNY PRETRE <fanny.pretre@gmail.com>
2025-06-27 15:56:20 +0200

    Admin panel ok : see and deleting food working

--- adminpanel/src/service/foodService.js
+++ adminpanel/src/service/foodService.js
@@ -15,3 +15,24 @@ export const addFood = async (foodData, image) => {
     throw error;
   }
 };
+
+export const getFoodList = async () => {
+  try {
+    const response = await axios.get(API_URL);
+
+    return response.data;
+  } catch (error) {
+    console.log("Error fetching food list", error);
+    throw error;
+  }
+};
+
+export const deleteFood = async (foodId) => {
+  try {
+    const response = await axios.delete(API_URL + "/" + foodId);
+    return response.status === 204;
+  } catch (error) {
+    console.log("Error while deleting the food", error);
+    throw error;
+  }
+};
